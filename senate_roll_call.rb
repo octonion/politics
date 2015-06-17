50e0e6d1085c39147c3149b2c1f44562229e587c
Christopher D. Long <galizur@gmail.com>
2015-06-17 19:02:02 -0400

    Pull out all URLs.

--- senate_roll_call.rb
+++ senate_roll_call.rb
@@ -6,28 +6,63 @@ require 'mechanize'
 agent = Mechanize.new{ |agent| agent.history.max_size=0 }
 agent.user_agent = 'Mozilla/5.0'
 
-congress_num = ARGV[0]
-session_num = ARGV[1]
+congress_id = ARGV[0]
+session_id = ARGV[1]
 
-print "Congress #{congress_num}, session #{session_num} -"
+print "Congress #{congress_id}, session #{session_id} -"
 
-url = "http://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_#{congress_num}_#{session_num}.htm"
+base_url = "http://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_#{congress_id}_#{session_id}.htm"
 
-agreed = CSV.open("csv/agreed_#{congress_num}_#{session_num}.csv", "w")
+agreed = CSV.open("csv/agreed_#{congress_id}_#{session_id}.csv", "w")
+
+header = ["congress_id", "session_id",
+          "vote", "vote_url",
+          "result",
+          "question_description",
+          "reference_issue", "reference_issue_url",
+          "issue", "issue_url",
+          "date"]
+
+agreed << header
 
 path = '//td[2][text()="Agreed to"]/..'
 
-page = agent.get(url)
+page = agent.get(base_url)
 
 found = 0
 page.parser.xpath(path).each do |tr|
 
-  row = [congress_num, session_num]
+  row = [congress_id, session_id]
 
   tr.xpath("td").each_with_index do |td,j|
 
-    text = td.text.scrub.strip rescue nil
-    row += [text]
+    text = td.text.gsub("&nbsp;", " ").scrub.strip rescue nil
+
+    case j
+    when 0,3
+
+      a = td.xpath("a")
+      relative_url = a.first.attributes["href"].to_s rescue nil
+      text_url = a.inner_text.gsub("&nbsp;", " ").scrub.strip rescue nil
+      full_url = URI.join(base_url, relative_url).to_s rescue nil
+      
+      row += [text_url, full_url]
+
+    when 2
+
+      a = td.xpath("a")
+      relative_url = a.first.attributes["href"].to_s rescue nil
+      text_url = a.inner_text.gsub("&nbsp;", " ").scrub.strip rescue nil
+      full_url = URI.join(base_url, relative_url).to_s rescue nil
+
+      row += [text, text_url, full_url]
+      
+    else
+      
+      row += [text]
+      
+    end
+
   end
   
   agreed << row
@@ -36,41 +71,3 @@ end
 
 agreed.close
 print " #{found}\n"
-
-=begin
-        case j
-        when 1,2
-          team = td.xpath("a").first.text.scrub.strip rescue nil
-          href = td.xpath("a").first.attributes["href"].to_s
-          team_id = href.split("=")[1].split("&")[0] rescue nil
-          href = href.scrub.strip rescue nil
-          team_url = URI.join(url, href).to_s
-          score = td.xpath("b").first.text rescue nil
-          row += [team_id, team, team_url, score]
-        when 5
-          text = td.text.scrub.strip rescue nil
-          score = text.split("\t")[0].scrub.strip rescue nil
-          ot = text.split("\t")[1].scrub.strip rescue nil
-          row += [score, ot]
-        when 6
-          flash_url = nil
-          boxscore_url = nil
-          game_id = nil
-          td.xpath("a").each_with_index do |a,k|
-            case k
-            when 0
-              href = a.attributes["href"].to_s
-              game_id = href.split("=")[1]
-              href = href.scrub.strip rescue nil
-              flash_url = URI.join(url, href).to_s
-            when 1
-              href = a.attributes["href"].to_s
-              href = href.scrub.strip rescue nil
-              boxscore_url = URI.join(url, href).to_s
-            end
-          end
-          row += [game_id, flash_url, boxscore_url]
-        else
-          row << td.text.scrub.strip rescue nil
-        end
-=end
